6850134fd18c140c2fed236f50e689fff1e09348
Abhishek Kaudare <2016.abhishek.kaudare@ves.ac.in>
2018-03-04 19:00:17 +0530

    Admin Set

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Stock Manager</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <!-- <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"> -->
+    <!-- Bootstrap 3.3.7 -->
+    <!-- <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css"> -->
+    <!-- Font Awesome -->
+    <!-- <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css"> -->
+    <!-- Ionicons -->
+    <!-- <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css"> -->
+    <!-- Theme style -->
+    <!-- <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css"> -->
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+    <!-- <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css"> -->
+    <!-- Pace style -->
+    <!-- <link rel="stylesheet" href="../../plugins/pace/pace.min.css"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif] -->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet" href="http://ppa.com/css/app.css">
+
+</head>
+
+<body class="skin-blue sidebar-mini sidebar-open" style="height: auto; min-height: 100%;">
+    <div class="wrapper" style="height: auto; min-height: 100%;">
+
+
+        @include('layouts.header');
+
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+                <!-- Sidebar user panel -->
+        
+                <!-- search form -->
+        
+                <!-- /.search form -->
+                <!-- sidebar menu: : style can be found in sidebar.less -->
+                <ul class="sidebar-menu tree" data-widget="tree">
+                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-caret-square-o-down"></i>
+                            <span>Inwards</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-caret-square-o-up"></i>
+                            <span>Outwards</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-archive"></i>
+                            <span>Stock</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="min-height: 497.933px;">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    Stock
+                </h1>
+
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+
+                <!--/To Be Constructed-->
+                Main Construction Section
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 2.4.0
+            </div>
+            <strong>Stock Manager</strong>
+        </footer>
+
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery 3 -->
+    <!-- <script src="../../bower_components/jquery/dist/jquery.min.js"></script> -->
+    <!-- Bootstrap 3.3.7 -->
+    <!-- <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
+    <!-- FastClick -->
+    <!-- <script src="../../bower_components/fastclick/lib/fastclick.js"></script> -->
+    <!-- AdminLTE App -->
+    <!-- <script src="../../dist/js/adminlte.min.js"></script> -->
+    <!-- AdminLTE for demo purposes -->
+    <!-- <script src="../../dist/js/demo.js"></script> -->
+    <script src="http://ppa.com/js/app.js"></script>
+
+
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.balde.php
@@ -0,0 +1,33 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+
+        <!-- search form -->
+
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu tree" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-caret-square-o-down"></i>
+                    <span>Inwards</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-caret-square-o-up"></i>
+                    <span>Outwards</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-archive"></i>
+                    <span>Stock</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
